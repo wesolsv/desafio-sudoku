da036d2462f5273a0e60e045185191539d33c4a6
Wesley Oliveira Silva <103519823+wesolsv@users.noreply.github.com>
2025-03-05 15:44:52 -0300

    iniciando projeto do jogo

---
+++ src/br/com/wszd/model/GameStatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.wszd.model;
+
+public enum GameStatusEnum {
+
+    NON_STARTED,
+    INCOMPLETE,
+    COMPLETE
+}
---
+++ src/br/com/wszd/util/BoardTemplate.java
@@ -0,0 +1,9 @@
+package br.com.wszd.util;
+
+public final class BoardTemplate {
+
+    private BoardTemplate() {}
+
+    public final static String BOARD_TEMPLATE = "";
+
+}
